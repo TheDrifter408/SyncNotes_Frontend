be34f4c5317e0b8f6c6eba68ed1445b60ef8f3dc
TheDrifter408 <nurullahan408@gmail.com>
2026-03-13 12:49:32 +0600

    Added JWT strategies and configured config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { Prisma } from './prisma/prisma.service';
 import { NotesModule } from './notes/notes.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.development.local',
     }),
-    NotesModule
+    NotesModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService, Prisma],
